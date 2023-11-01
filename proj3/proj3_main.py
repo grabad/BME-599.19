071cfaf8f16992d30e75b6042daa2b4b35f70bc0
grabad <george.a.rabadi@gmail.com>
2023-10-31 20:09:34 -0400

    starting 4

--- proj3/proj3_main.py
+++ proj3/proj3_main.py
@@ -1,15 +1,56 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import scipy.signal as scs
+import seaborn as sns
+
+sns.set()
 
 ## 1. How long is the recording in seconds? 
+raw_data = loadmat('proj3/data.mat')
+
+data = raw_data['data']
+fs = raw_data['fs'][0][0]
+stim = raw_data['stim']
+
+data_length = np.size(data, axis=1)
+time_length = data_length/fs
+
+print(f'Data is {time_length} seconds long\n')
 
 ## 2. For each channel, evaluate its mean and standard deviation? 
 
+means = np.mean(data, axis=1)
+stdevs = np.std(data, axis=1)
+
+for i in range(5):
+    print(f'Channel {i+1}: Mean={means[i]}, STD={stdevs[i]}')
+print()
+
 ## 3. Evaluate the correlation between every pair of different channels. 
+pairs =  np.array([[0, 1],
+          [0, 2],
+          [0, 3],
+          [0, 4],
+          [1, 2],
+          [1, 3],
+          [1, 4],
+          [2, 3],
+          [2, 4],
+          [3, 4]])
+
+coefs = np.zeros(10)
+
+for i in range(10):
+    coefs[i] = np.corrcoef(data[pairs[i,0]], data[pairs[i,1]])[0, 1]
+    print(f'Correlation between Channel {pairs[i,0]+1} and {pairs[i,1]+1}: {coefs[i]}')
+print()
 
 ## 4. For each channel, detect the neuronal spikes. Hint: this is an open-ended question. 
 ## You need to observe the data first and come up with your own criteria for a spike. 
 ## Typically, a spike will show a peak that deviates from the baseline (often just the mean) by a 
 ## notable extent (e.g., multiple times of a standard deviation). The peak may be either positive or negative. Exercise your own judgment. 
+
 
 ## 5. For each spike you have detected in 4, choose a short window around the peak of the spike. 
 ## The window is often very short. You may explore the data and exercise your own judgment. 
